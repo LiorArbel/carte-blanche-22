fef8c6dcc112806b130c9a8e5180b1614664e9b2
Lior B <lliorarbell@gmail.com>
2022-06-17 20:19:12 +0300

    ping and shit

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,13 +6,14 @@ import Controller from './routes/controller';
 import _ from 'lodash';
 
 let exampleSocket;
-const publicUrl = process.env.REACT_APP_PUBLIC_URL || "localhost:8081";
-// const publicUrl = process.env.REACT_APP_PUBLIC_URL || "carte-blanche22-server.azurewebsites.net";
+// const publicUrl = process.env.REACT_APP_PUBLIC_URL || "localhost:8081";
+const publicUrl = process.env.REACT_APP_PUBLIC_URL || "carte-blanche22-server.azurewebsites.net";
 let aud = new Audio("/15step.mp3");
 aud.preload = "auto";
 
 function App() {
   const [recievedPlay, setRecievedPlay] = useState(false);
+  const [ping, setPing] = useState(0);
   const [color, setColor] = useState("#000000");
 
   const sendColor = (c) => {
@@ -27,13 +28,22 @@ function App() {
     exampleSocket = new WebSocket("wss://" + publicUrl,);
     exampleSocket.onopen = (e) => {
       console.log("connected successfully!!!");
+      pingLoop(exampleSocket);
     }
     exampleSocket.onmessage = (e) => {
       const data = JSON.parse(e.data);
       console.log(data);
+      if(data.type === "ping"){
+        setPing(() => (ping + ((new Date()).getTime() - (new Date(data.startTime)).getTime()))/2);
+      }
       if (data.music) {
+        if(data.music === "playWithCorrection"){
+          aud.play();
+          aud.currentTime = ping/2/1000;
+        }
         if (data.music === "play") {
           aud.play();
+          aud.currentTime = 0;
         } else if (data.music === "stop") {
           aud.pause();
           aud.currentTime = 0;
@@ -45,12 +55,16 @@ function App() {
         setColor(data.color);
       }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
     <div className="App">
       <header style={{ backgroundColor: color }} className="App-header">
         <p>
           server address is {publicUrl}.
+        </p>
+        <p>
+          ping is {ping}
         </p>
         {recievedPlay && <p>
           recieved a play request!
@@ -62,6 +76,7 @@ function App() {
             <Route path="controller" element={
               <Controller
                 play={() => exampleSocket.send(JSON.stringify({ music: "play" }))}
+                playWithCorrection={() => exampleSocket.send(JSON.stringify({ music: "playWithCorrection" }))}
                 stop={() => exampleSocket.send(JSON.stringify({ music: "stop" }))}
                 setColor={(c) => {
                   setColor(c);
@@ -75,6 +90,11 @@ function App() {
       </header>
     </div>
   );
+}
+
+function pingLoop(socket:WebSocket){
+  socket.send(JSON.stringify({type: "ping", startTime: new Date()}));
+  setTimeout(() => pingLoop(socket), 500);
 }
 
 export default App;
--- front/src/routes/controller.js
+++ front/src/routes/controller.js
@@ -1,8 +1,9 @@
 import { HexColorPicker } from "react-colorful";
 
-export default function Controller({ play, stop, color, setColor }) {
+export default function Controller({ play, playWithCorrection, stop, color, setColor }) {
     return <div>
         <button onClick={play}>Play</button>
+        <button onClick={playWithCorrection}>play with correction</button>
         <button onClick={stop}>Stop</button>
         <HexColorPicker color={color} onChange={setColor} />
     </div>
